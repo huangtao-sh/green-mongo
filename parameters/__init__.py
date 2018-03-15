0becbc572e2b99b8182d65baf66a15cda764c81e
huangtao-sh <huangtao.jh@gmail.com>
2018-03-15 21:24:05 +0800

    init

---
+++ parameters/__init__.py
@@ -0,0 +1,11 @@
+# 项目：参数管理
+# 模块：基础模块
+# 作者：黄涛
+# License:GPL
+# Email:huangtao.sh@icloud.com
+# 创建：2016-08-13 19:05
+
+'''
+本包主要用于创建参数的相关模块。
+'''
+from mongo.document import *
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#  项目：生产管理平台
+#  作者：黄涛
+#  创建：2018-3-15
+
+from setuptools import setup,find_packages
+from orange import get_ver
+
+consoles=[]
+guis=[]
+
+setup(
+        name='green-mongo',
+        version=get_ver(),
+        author='huangtao',
+        author_email='hunto@163.com',
+        platforms='any',
+        description='work platform',
+        long_description='work platform',
+        install_requires=None,
+        entry_points={
+            'gui_scripts':guis,
+            'console_scripts':consoles},
+        packages=find_packages(exclude=['testing']),
+        url='https://github.com/huangtao-sh/mongo.git',
+        license='GPL',
+        )
