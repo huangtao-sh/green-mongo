1b13e490215cd5afd950601354382d2ded4db62f
huangtao-sh <huangtao.jh@gmail.com>
2018-05-07 15:56:11 +0800

    upgrade

--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version="0.0.1a24"
+version="0.0.1a25"
--- parameters/zh.py
+++ parameters/zh.py
@@ -65,12 +65,13 @@ def main(ac=None):
         elif R/r'\d{6}\-\d{1,3}':
             km, xh = ac.split('-')
             for obj in AcTemplate.objects((P.km == km) & (P.xh == int(xh))):
-                print(obj.jglx, obj.km, obj.xh, obj.bz, obj.sxrq, obj.hm)
+                print(obj.jglx, obj.km, obj.xh, obj.bz,
+                      obj.sxrq, obj.hm, obj.tzed)
 
 
 if __name__ == '__main__':
     file = max(Path('d:/工作/参数备份').rglob('ggnbzhmb.del'))
     print(file)
     from orange.coroutine import run
-    run(AcTemplate.amport_file(file,drop=True,encoding='gbk'))
-    obj=AcTemplate.objects.first()
+    run(AcTemplate.amport_file(file, drop=True, encoding='gbk'))
+    obj = AcTemplate.objects.first()
