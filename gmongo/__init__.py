fb78f1cb094f767efe0d522eba3dc05e0af4d729
huangtao-sh <huangtao.jh@gmail.com>
2018-05-25 11:33:34 +0800

    upgrade

--- gmongo/__init__.py
+++ gmongo/__init__.py
@@ -1,3 +1,5 @@
-from .__version__ import version  as __version__
+from .__version__ import version as __version__
+from orange import R, Path
+WORKPATH = Path('~/Documents/工作')
 
-__all__='__version__',
+__all__ = '__version__', 'R', 'Path', "WORKPATH"
--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version="0.0.1a30"
+version="0.0.1a31"
