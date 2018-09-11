84d3956300e995a58a5c1e9b2e65ef13f894c8be
huangtao-sh <huangtao.jh@gmail.com>
2018-09-11 19:03:44 +0800

    add user.py

--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version = "0.0.1a69"
+version = "0.0.1a70"
---
+++ params/user.py
@@ -0,0 +1,11 @@
+# 项目：工作平台
+# 模块：用户
+# 作者：黄涛
+# License:GPL
+# Email:huangtao.sh@icloud.com
+# 创建：2018-09-11 18:52
+
+from glemon import Document, Descriptor
+
+class Teller(Document):
+    _projects='_id','name','telephone','grade','branch','userid'
