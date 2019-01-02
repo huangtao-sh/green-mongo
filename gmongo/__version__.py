022c1f31b71479edfbede68d9c4995b270f73d16
huangtao-sh <huangtao.jh@gmail.com>
2019-01-02 20:37:39 +0800

    upgrade

--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version = "0.0.1b27"
+version = "0.0.1b28"
--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@
 from orange import setup
 
 consoles = [
-    'jym=trans:JyJiaoyi.main',
-    'sjdr=params.sjdr:sjdr',
-    'jgm=params.jgm:GgJgm.main',
-    'ac=params.zh:main',
-    'km=params.accounting:Accounting.run',
+    'jym=trans:JyJiaoyi.main',               # 交易码表
+    'sjdr=params.sjdr:sjdr',                 # 数据导入
+    'jgm=params.jgm:GgJgm.main',             # 机构码表
+    'ac=params.zh:main',                     # 内部账户
+    'km=params.accounting:Accounting.run',   # 科目表
     'fhlz=lvzhi.fhlz:main',                  # 分行运营主管履职报告
     'lz=lvzhi:main',                         # 营业主管履职报告
     # 'jqb=vacation:Vacation.main',
     'lxr=params.branch:Contacts.main',       # 查询联系人
-    'teller=params.user:Teller.main',
-    'bz=params.bz:GgBzb.main',
-    'ed=params.dengji:EduDengji.main',
-    'jq=vacation:Holiday.main',
-    'wh=params.paijia:PaiJia.main',
-    'xxbm=params.zhxxbm:Zhxxbm.main',
+    'teller=params.user:Teller.main',        # 柜员表
+    'bz=params.bz:GgBzb.main',               # 币种
+    'ed=params.dengji:EduDengji.main',       # 额度配置
+    'jq=vacation:Holiday.main',              # 假期表
+    'wh=params.paijia:PaiJia.main',          # 外汇
+    'xxbm=params.zhxxbm:Zhxxbm.main',        # 性质编码
 ]
 
 setup(
