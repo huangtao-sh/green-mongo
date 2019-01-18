8072b33909459d1b63aad90b2593d7865459eb5b
huangtao-sh <huangtao.jh@gmail.com>
2019-01-18 13:51:48 +0800

    代码优化

--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version = "0.0.1b41"
+version = "0.0.1b42"
--- gmongo/zfbb/rpt.py
+++ gmongo/zfbb/rpt.py
@@ -15,7 +15,7 @@ def percent(b, t):
 
 
 SQL = ('select a."id",sum(b.vv),sum(vv2) from parameter a '
-       'left join PaymentData b on a."in"=b."in" and a.dn=b.dn '
+       'join PaymentData b on a."in"=b."in" and a.dn=b.dn '
        'where b.at="CITY" and '
        '((a.rule=0 and b.subno=?) or (a.rule=1 and b.subno between ? and ?)) '
        'group by a."in",a.dn '
@@ -26,9 +26,9 @@ def export(qc):
     ensure(QcPattern == qc, '期次的格式应为：YYYY-Q')
     year, q = qc.split('-')
     d = int(year)*12+(int(q)-1)*3-12
-    months = [f'{m//12}{m%12+1:02d}' for m in range(d, d+15)]
-    data = defaultdict(lambda: [])
-    for i in (14, 2, 11):
+    months = [f'{m//12}{m%12+1:02d}' for m in range(d, d+15)]  # 生成 15 个连续的月份
+    data = defaultdict(lambda: [])                             # 初始化数据
+    for i in (14, 2, 11):                                      # 分别获取当期、同比、环节数据
         for r in find(SQL, [months[i], months[i-2], months[i]]):
             data[r[0]].extend(r[1:])
     for k, v in sorted(data.items()):
