c0e0f0838418310817cd0ec2e00aa39ca5990c34
huangtao-sh <huangtao.jh@gmail.com>
2019-06-13 15:17:28 +0800

    upgrade

--- gmongo/params/__init__.py
+++ gmongo/params/__init__.py
@@ -6,10 +6,25 @@
 # 创建：2019-05-14 11:36
 
 from orange.utils.sqlite import executefile, fetch, db_config, loadcheck, insert, execute,\
-    fetch,fetchone,fetchvalue,transaction
-from orange import HOME
+    fetch, fetchone, fetchvalue, transaction
+from orange import HOME, Path
 from functools import wraps
 
 db_config('params')
 executefile('gmongo', 'sql/params.sql')
 ROOT = HOME / 'OneDrive/工作/参数备份'
+
+
+def load_file(path: Path, table: str, drop: bool = True, proc: 'function' = None, **kw):
+    @loadcheck
+    def _(path: Path):
+        if drop:
+            execute(f'delete from {table}')
+        if callable(proc):
+            data = proc(path, **kw)
+        else:
+            data = path.iter_csv(**kw)
+        insert(table, data)
+        print(f'{path.name} 导入成功')
+
+    return _(path)
--- gmongo/params/branch.py
+++ gmongo/params/branch.py
@@ -5,26 +5,16 @@
 # Email:   huangtao.sh@icloud.com
 # 创建：2019-05-14 14:01
 
-from gmongo.params import loadcheck, insert, ROOT, execute, fetch, fetchvalue, transaction
+from . import loadcheck, insert, ROOT, execute, fetch, fetchvalue, transaction, load_file
 from orange import Path, R, extract, arg, tprint
 from orange.utils.sqlite import fix_db_name
+from functools import partial
 
 
 def loadfile():
     path = ROOT.find('ggjgm.del')
-
-    @loadcheck
-    def _(path: Path):
-        execute('delete from ggjgm')
-        insert(
-            'ggjgm',
-            path.iter_csv(encoding='gbk',
-                          errors='ignore',
-                          converter={1: str.strip},
-                          columns=(0, 1, 3, 7, 15, 16, 17)))
-        print(f'{path.name} 导入成功')
-
-    return _(path)
+    return load_file(path, 'ggjgm', encoding='gbk', errors='ignore', converter={1: str.strip},
+                     columns=(0, 1, 3, 7, 15, 16, 17))
 
 
 BranchPattern = R / '(总行|.{2}分行)'
