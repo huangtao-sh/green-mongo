4389a515d02c6e3acba10f2a49500fd6dff1b246
huangtao-sh <huangtao.jh@gmail.com>
2019-10-16 20:55:22 +0800

    upgrade

--- gmongo/params/branch.py
+++ gmongo/params/branch.py
@@ -22,19 +22,19 @@ def loadfile():
                      columns=(0, 1, 3, 7, 15, 16, 17))
 
 
-BranchPattern = R / '(总行|.{2}分行)'
+BranchPattern = R / '浙商银行(.*?公司)?(.*?行).*?'
 
 
 def get_branches():
     '获取机构对应分行的名称'
     sql = 'select a.jgm,b.mc from ggjgm a left join ggjgm b on a.hzjgm=b.jgm'
-    def convert(obj): return (obj[0], extract(obj[1], BranchPattern, 1))
+
+    def convert(obj):
+        return (obj[0], extract(obj[1], BranchPattern, 2))
+
     conn = connect('~/Onedrive/db/params.db')
     with closing(conn):
-        branches = dict(map(convert, conn.fetch(sql)))
-        branches['331000000'] = '总行业务处理中心'
-        branches['331000808'] = '总行营业中心'
-        return branches
+        return dict(map(convert, conn.fetch(sql)))
 
 
 @arg('query', help='查询条件')
