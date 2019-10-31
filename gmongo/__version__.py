61308ee110f4a72cdf5b2e7b2afdd7b745d9b9eb
huangtao-sh <huangtao.jh@gmail.com>
2019-10-31 11:22:35 +0800

    upgrade

--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version = "0.0.2a48"
+version = "0.0.2a49"
--- gmongo/params/kemu.py
+++ gmongo/params/kemu.py
@@ -11,27 +11,32 @@ from gmongo.params import get_param_ver
 from gmongo.__version__ import version
 
 
+def list_kemu(sql):
+    print(*fetch(sql), sep='\t')
+
+
+def show_kemu(sql, arg=None):
+    data = fetchone(sql, arg)
+    if data:
+        print('%s\t%s\n%s' % (data))
+        return True
+
+
 @arg('query', help='查询科目信息')
 def main(query):
     ver = get_param_ver('kemu')[0]
     print('程序版本：', version)
     print('数据版本：', ver, end='\n\n')
     if R / r'\d{4}' == query:
-        data = fetchone('select * from kemu where km=?', [query])
-        if data:
-            print('%s\t%s\n%s' % (data))
+        if show_kemu('select * from kemu where km=?', [query]):
             print('\n子科目列表')
-            for row in fetch(
-                    f'select km,name from kemu where km like "{query}__" order by km'):
-                print(*row, sep='\t')
+            list_kemu(
+                f'select km,name from kemu where km like "{query}__" order by km'
+            )
     elif R / r'\d{6}' == query:
-        data = fetchone('select * from kemu where km=?', [query])
-        if data:
-            print('%s\t%s\n%s' % (data))
+        if show_kemu('select * from kemu where km=?', [query]):
+            ...
     elif R / r'\d{1,5}' == query:
-        for r in fetch(f'select km,name from kemu where km like "{query}%"'):
-            print(*r, sep='\t')
+        list_kemu(f'select km,name from kemu where km like "{query}%"')
     else:
-        for r in fetch(
-                f'select km,name from kemu where name like "%{query}%"'):
-            print(*r, sep='\t')
+        list_kemu(f'select km,name from kemu where name like "%{query}%"')
