fc5f28763a935e124257407715f5adf109be1412
huangtao-sh <huangtao.jh@gmail.com>
2019-10-31 15:56:29 +0800

    upgrade

--- gmongo/__version__.py
+++ gmongo/__version__.py
@@ -1,1 +1,1 @@
-version = "0.0.2a51"
+version = "0.0.2a52"
--- gmongo/params/teller.py
+++ gmongo/params/teller.py
@@ -5,7 +5,7 @@
 # Email:   huangtao.sh@icloud.com
 # 创建：2019-10-31 14:37
 
-from gmongo.params import load_file, ROOT, fetchone
+from gmongo.params import load_file, ROOT, fetchone, fetch
 from orange import R, arg, tprint
 
 
@@ -23,15 +23,18 @@ def load_teller():
     return load_file(path, 'teller', proc=read)
 
 
-def show_teller(teller):
-    tlr = fetchone('select * from teller where id=?', [teller])
+def show_teller(sql, arg):
     header = '柜员号，姓名，电话，柜员级别，柜组，机构号，员工号，岗位，执行交易组，转账限额，现金限额，认证类型，状态，屏蔽交易，岗位性质，启用日期，停用日期，交易币种，发起交易组，证件种类，证件号码'.split(
         '，')
-    if tlr:
+    print(sql)
+    for tlr in fetch(sql, arg):
         tprint(zip(header, tlr), {0: '15'})
+        print()
 
 
-@arg('teller', help='柜员号')
-def main(teller=None):
-    if teller:
-        show_teller(teller)
+@arg('query', help='查询条件')
+def main(query=None):
+    if R / r'\d{5}' == query:
+        show_teller('select * from teller where id=?', [query])
+    elif R / r'[A-Z]{1,2}\d{4}'==query:
+        show_teller('select * from teller where userid=?', [query])
