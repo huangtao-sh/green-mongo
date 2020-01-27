aac5d3ab321fe65f8dab1ca1e4518a232e1b17b7
huangtao-sh <huangtao.jh@gmail.com>
2020-01-27 09:46:42 +0800

    upgrade

--- test2.py
+++ test2.py
@@ -1,1 +1,11 @@
-from params.load import *
+from glemon import config,P
+config(is_dev=False)
+from vacation import Holiday
+
+obj= Holiday.find(P._id=='2020').first()
+
+anpai=obj.anpai
+anpai[1]='二、春节：1月24日至2月2日放假调休，共10天。1月19日（星期日）上班。'
+obj.anpai=anpai
+obj.save()
+
